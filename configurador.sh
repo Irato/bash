50fc7a6c098149b4e396065f33072bc4ddf3c727
Irato <lurofr@gmail.com>
2016-10-16 22:32:37 -0200

    Adicionado as funções de NAT64 e DNS64 no configurador principal

--- configurador.sh
+++ configurador.sh
@@ -11,6 +11,8 @@ let contador+=1
 done
 fi
 
+#Criacao das  funcoes
+
 #Configuracao manual IPV4
 
 function config_if(){
@@ -385,20 +387,297 @@ dialog --title "Configuracao automatica da topologia IPV6" \
         esac
 }
 
-#Criacao das Funcoes
+#Funcao do tayga-NAT64
+
+function conf_NAT64(){
+
+ dialog	--title "Configuracao Manual Tayga" \
+		--menu "Insira os parametros de configuracao" 0 0 0 \
+	        "Prefixo IPv6 de NAT64" "$PREFIX" \
+	        "Pool de mapeamento IPv4" "$TAYGA_IPV4_POOL" \
+	        "Endereço IPv4 do Tayga" "$TAYGA_IPV4ADDR" \
+	        "Endereço da interface IPv4" "$ifacev4" \
+	        "Endereço da interface IPv6" "$ifacev6" \
+			"Iniciar config. do NAT64" '' 2> /tmp/opt
+		opt=$(cat /tmp/opt)
+
+case $opt in
+			"Prefixo IPv6 de NAT64")
+				dialog	--title "Prefixo NAT64" \
+					--inputbox "Favor digitar o prefixo NAT64 com a máscara (/96) (ex.: 2001:db8:1:ffff::/96)" 0 0 2>/tmp/prefixnat64.conf
+					PREFIX=$(cat /tmp/prefixnat64.conf)
+					conf_NAT64
+				;;
+
+			"Pool de mapeamento IPv4")
+				dialog	--title "Config. de pool NAT64" \
+					--inputbox "Favor Digitar um Endereco IPv4 de rede com a máscara CIDR (ex.: 192.168.255.0/24)" 0 0 2>/tmp/poolnat64.conf
+					 TAYGA_IPV4_POOL=$(cat /tmp/poolnat64.conf)
+					conf_NAT64
+				;;
+
+			"Endereço IPv4 do Tayga")
+				dialog	--title "Config.  de endereço" \
+					--inputbox "Favor Digitar um Endereco IPv4 pertencente ao pool de endereços do NAT64 (ex.: 192.168.255.1)" 0 0 2>/tmp/ipv4nat64.conf
+					TAYGA_IPV4ADDR=$(cat /tmp/ipv4nat64.conf)
+					conf_NAT64
+				;;
+
+			"Endereço da interface IPv4")
+				dialog	--title "Config.  de endereço" \
+					--inputbox "Favor digitar o endereço da interface IPv4 do roteador COM a máscara (ex.: 192.168.0.200/24)" 0 0 2>/tmp/ifacev4.conf
+					ifacev4=$(cat /tmp/ifacev4.conf)
+					echo "${ifacev4%???}" > /tmp/v4nomask
+					ipv4nomask=$(cat /tmp/v4nomask)
+					conf_NAT64
+				;;
+
+			"Endereço da interface IPv6")
+				dialog	--title "Config.  de endereço" \
+					--inputbox "Favor digitar o endereço da interface IPv6 do roteador COM a máscara (ex.: 2001:db8:1::3/64)" 0 0 2>/tmp/ifacev6.conf
+					ifacev6=$(cat /tmp/ifacev6.conf)
+					echo "${ifacev6%???}" > /tmp/v6nomask
+					ipv6nomask=$(cat /tmp/v6nomask)
+					conf_NAT64
+				;;
+
+			"Iniciar config. do NAT64")
+				dialog --yesno 'Deseja realizar SNAT para a rede externa?' 0 0
+				doNAT=$?
+				configIP
+				configNAT64
+				;;
+			esac
+		}
+
+ configIP(){
+
+ 	sysctl -w net.ipv6.conf.all.forwarding=1
+	sysctl -w net.ipv4.ip_forward=1
+
+ 	# Interface IPv4 #####
+ 	sudo ip addr flush dev ${interface[1]}
+	sudo ip link set ${interface[1]} up 
+	sudo ip addr add $ifacev4 dev ${interface[1]}
+
+	# Interface IPv6 #####
+	sudo ip addr flush dev ${interface[2]}
+	sudo ip link set ${interface[2]} up 
+	sudo ip -6 addr add $ifacev6 dev ${interface[2]}
+ }
+
+
+ configNAT64(){
+
+#	DIR_TAYGA=$(find /home -type d -name tayga-0.9.2)
+
+#	cd $DIR_TAYGA
+
+#	./configure && make && make install
+
+#	mkdir -p /var/db/tayga
+
+	echo "tun-device nat64
+		ipv4-addr $TAYGA_IPV4ADDR
+		prefix $PREFIX
+		dynamic-pool $TAYGA_IPV4_POOL
+		data-dir /var/db/tayga" > /usr/local/etc/tayga.conf
+
+	tayga --mktun
+	ip link set nat64 up
+	ip addr add $ifacev4 dev nat64 
+	ip addr add $ifacev6 dev nat64   
+	ip route add $PREFIX dev nat64
+	ip route add  $TAYGA_IPV4_POOL dev nat64
+	
+	iptables -F 
+	
+	if [ $doNAT = 0 ]; then
+		iptables -t nat -F 
+		iptables -t nat -A POSTROUTING -s $TAYGA_IPV4_POOL -o ${interface[1]} -j SNAT --to $ipv4nomask
+	fi
+	
+	iptables -A FORWARD -i ${interface[1]} -o nat64 -m state --state RELATED,ESTABLISHED -j ACCEPT 
+	iptables -A FORWARD -i nat64 -o ${interface[1]} -j ACCEPT
+
+	echo -e "\n************* Configuração NAT64 completa! *******************\n"
+}
+
+ PREFIX="-"
+ TAYGA_IPV4ADDR="-"
+ TAYGA_IPV4_POOL="-"
+ ifacev6="-"
+ ifacev4="-"
+
+
+################### CONFIGURAÇÃO SERVIDOR DNS64 ##########################
+
+function conf_DNS64(){
+
+dialog	--title "Configuração DNS64" \
+		--menu "Insira os parametros de configuração" 0 0 0 \
+	        "Forwarder IPv4" "$v4Forwarder" \
+ 	        "Forwarder IPv6" "$v6Forwarder" \
+ 	        "Prefixo do NAT64" "$PREFIXNAT64" \
+ 	        "Endereço da interface IPv4" "$ifacev4" \
+ 	        "Rede IPv6 permitida" "$v6network" \
+			"Iniciar config. do DNS64" '' 2> /tmp/opt
+		opt=$(cat /tmp/opt)
+
+
+	case $opt in
+			"Forwarder IPv4")
+				dialog	--title "Forwarder IPv4" \
+					--inputbox "Favor digitar o Forwarder IPv4 (ex.: 8.8.8.8)" 0 0 2>/tmp/forwarderv4.conf
+					v4Forwarder=$(cat /tmp/forwarderv4.conf)
+			conf_DNS64		
+				;;
+
+			"Forwarder IPv6")
+				dialog	--title "Forwarder IPv6" \
+					--inputbox "Favor digitar o Forwarder IPv6 (ex.: 2001:4860:4860::8888)" 0 0 2>/tmp/forwarderv6.conf
+					v6Forwarder=$(cat /tmp/forwarderv6.conf)
+					conf_DNS64
+				;;
+
+			"Prefixo do NAT64")
+				dialog	--title "Config.  de prefixo NAT64" \
+					--inputbox "Favor digitar o prefixo utilizado pelo NAT64 (ex.: 2001:db8:1:ffff::/96)" 0 0 2>/tmp/prefixnat64.conf
+					PREFIXNAT64=$(cat /tmp/prefixnat64.conf)
+					conf_DNS64
+				;;
+
+			"Rede IPv6 permitida")
+				dialog	--title "Config.  de prefixo NAT64" \
+					--inputbox "Favor digitar o prefixo utilizado pelo NAT64 (ex.: 2001:db8::/64)" 0 0 2>/tmp/v6network.conf
+					v6network=$(cat /tmp/v6network.conf)
+					conf_DNS64
+				;;
+
+			"Endereço da interface IPv4")
+				dialog	--title "Config.  de endereço" \
+					--inputbox "Favor digitar o endereço da interface IPv4 do roteador COM a máscara (ex.: 192.168.0.200/24)" 0 0 2>/tmp/ifacev4.conf
+					ifacev4=$(cat /tmp/ifacev4.conf)
+					echo "${ifacev4%???}" > /tmp/v4nomask
+					ipv4nomask=$(cat /tmp/v4nomask)
+					conf_DNS64
+				;;
+
+			# "Endereço da interface IPv6")
+			# 	dialog	--title "Config.  de endereço" \
+			# 		--inputbox "Favor digitar o endereço da interface IPv6 do roteador COM a máscara (ex.: 2001:db8:1::3/64)" 0 0 2>/tmp/ifacev6.conf
+			# 		ifacev6=$(cat /tmp/ifacev6.conf)
+			# 		echo "${ifacev6%???}" > /tmp/v6nomask
+			# 		ipv6nomask=$(cat /tmp/v6nomask)
+			# 		menu
+			# 	;;
+
+			"Iniciar config. do DNS64")
+				dialog --yesno 'Deseja realizar autenticação DNSSEC?' 0 0
+				doDNSSEC=$?
+				dialog --yesno 'Deseja que o DNS responda autoritativamente ao receber respostas NXDOMAIN?' 0 0
+				doAUTORITATIVO=$?
+				dialog --yesno 'Deseja que o DNS64 retorne apenas endereços IPv4? (prefixo + ipv4)' 0 0
+				doEXCLUDE=$?
+				write_config
+				;;
+			esac
+		}
+write_config(){
+
+	if [ $doDNSSEC = 0 ]; then
+		
+		doDNSSECyn="yes"
+	
+	else
+
+		doDNSSECyn="no"
+
+	fi
+
+	if [ $doAUTORITATIVO = 0 ]; then
+		
+		doAUTORITATIVOyn="yes"
+	
+	else
+
+		doAUTORITATIVOyn="no"
+
+	fi
+
+	if [ $doEXCLUDE = 0 ]; then
+		
+		doEXCLUDEyn="exlude { any; };"
+	
+	else
+
+		doEXCLUDEyn="#exlude { any; };"
+
+	fi
+
+	echo  "options{
+	
+	directory \"var/cache/bind\"
+	
+		forwarders {
+			
+			$v4Forwarder ;
+			$v6Forwarder ;
+
+		};
+
+
+	dnssec-validation $doDNSSECyn ;
+
+	auth-nxdomain $doAUTORITATIVOyn ;    
+	
+	listen-on-v6 { any; }; 
+	
+	allow-query { localnets; localhost; $v6network ; }; 
+	
+	allow-recursion { localnets; localhost; $v6network ; }; 
+	
+	dns64 $PREFIXNAT64 { 
+	
+	clients { any; }; 
+	$doEXCLUDEyn
+
+	};
+	}" > /etc/bind/named.conf.options
+	
+
+	echo -e "\n\nFIM!!!!!!!!!\n\n"
+}
+
+v4Forwarder="-"
+v6Forwarder="-"
+PREFIXNAT64="-"
+ifacev4="-"
+ifacev6="-"
+v6network="-"
+
+#Criacao das  funcoes
 function voltar(){
 dialog	--title "Tela de Controle" \
-	--menu "Escolhe uma opcao:" 0 0 0 \
-	IP "Configuracao Manual" \
-	E "Configuracao dos hosts do experimento NAT64/DNS64" \
+	--menu "Escolha uma opcao:" 0 0 0 \
+	IP "Configuracao  Manual" \
+    "Experimento" "Configuracao automatica dos hosts do experimento NAT64/DNS64" \
+	NAT64 "Configuração do tayga no host D" \
+	DNS64 "Configuração do BIND no host D" \
 	VOLTAR '' 2> /tmp/opcao
 	opt=$(cat /tmp/opcao)
 	case $opt in
 		"IP")
 			config_if
 			;;
-		"E")
+		"Experimento")
 			conf_automatica	
+			;;
+		"NAT64")
+			conf_NAT64
+			;;
+		"DNS64")
+			conf_DNS64
 			;;
 		"VOLTAR")
 			voltar
@@ -408,20 +687,27 @@ dialog	--title "Tela de Controle" \
 
 #Menu Principal 
 dialog	--title "Tela de Controle" \
-	--menu "Escolhe uma opcao:" 0 0 0 \
+	--menu "Escolha uma opcao:" 0 0 0 \
 	IP "Configuracao  Manual" \
-        E "Configuracao dos hosts do experimento NAT64/DNS64" \
+    "Experimento" "Configuracao automatica dos hosts do experimento NAT64/DNS64" \
+	"NAT64" "Configuração do tayga no host D" \
+	"DNS64" "Configuração do BIND no host D" \
 	VOLTAR '' 2> /tmp/opcao
 	opt=$(cat /tmp/opcao)
 	case $opt in
 		"IP")
 			config_if
 			;;
-		"E")
+		"Experimento")
 			conf_automatica	
+			;;
+		"NAT64")
+			conf_NAT64
+			;;
+		"DNS64")
+			conf_DNS64
 			;;
 		"VOLTAR")
 			voltar
 			;;
 		esac
-
